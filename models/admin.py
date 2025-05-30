1eea2f53cbda4881540782aa792762a5a739ad7b
adel-nabih <86003399+adel-nabih@users.noreply.github.com>
2025-05-30 18:07:37 +0300

    completed gui

--- models/admin.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-
-class Admin(User):
-    def __init__(self, user_id, name):
-        super().__init__(user_id, name)
--- schema.sql
+++ schema.sql
@@ -1,8 +1,7 @@
--- Create the database
+--run in mysql workbench not here!!!!!!!!!
 CREATE DATABASE IF NOT EXISTS real_estate_db;
 USE real_estate_db;
 
--- Properties table
 CREATE TABLE IF NOT EXISTS properties (
     id INT AUTO_INCREMENT PRIMARY KEY,
     location VARCHAR(100) NOT NULL,
@@ -12,7 +11,6 @@ CREATE TABLE IF NOT EXISTS properties (
     status ENUM('available', 'sold') DEFAULT 'available'
 );
 
--- Clients table
 CREATE TABLE IF NOT EXISTS clients (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -23,14 +21,12 @@ CREATE TABLE IF NOT EXISTS clients (
         ON DELETE SET NULL ON UPDATE CASCADE
 );
 
--- Brokers table
 CREATE TABLE IF NOT EXISTS brokers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     years_experience INT DEFAULT 0
 );
 
--- Sales table
 CREATE TABLE IF NOT EXISTS sales (
     id INT AUTO_INCREMENT PRIMARY KEY,
     property_id INT NOT NULL,
@@ -45,28 +41,3 @@ CREATE TABLE IF NOT EXISTS sales (
     FOREIGN KEY (broker_id) REFERENCES brokers(id)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
-
--- CREATE TABLE IF NOT EXISTS users (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR(100) NOT NULL UNIQUE,
---     password_hash VARCHAR(255) NOT NULL,
---     role ENUM('admin', 'broker', 'client') NOT NULL
--- );
-
--- ALTER TABLE brokers
--- ADD COLUMN user_id INT UNIQUE,
--- ADD CONSTRAINT fk_broker_user FOREIGN KEY (user_id) REFERENCES users(id)
---     ON DELETE SET NULL ON UPDATE CASCADE;
-
--- ALTER TABLE clients
--- ADD COLUMN user_id INT UNIQUE,
--- ADD CONSTRAINT fk_client_user FOREIGN KEY (user_id) REFERENCES users(id)
---     ON DELETE SET NULL ON UPDATE CASCADE;
-
--- show tables;
-
--- ALTER TABLE users
--- ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-
--- INSERT INTO users (username, password_hash, role) 
--- VALUES ('admin1', '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewKyNiAYMyzJ/I6y', 'admin');
